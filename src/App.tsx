8c1e38ffdb07ab7ac487ad57db25f701cf7bb7cb
Krotesk <igor_98_zukov@mail.ru>
2023-08-14 19:58:50 +0300

    Список

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import Card, { CardVariant } from './components/Card';
+import UserList from './components/UserList';
+import { IUser } from './types/types';
 
 const App = () => {
+	const users: IUser[] = [
+		{id:1, name:'Igor', email:'test@mail.test', address:{city:"Moscow", street:"1 Street", zipcode:"11"}},
+		{id:2, name:'Igor', email:'test@mail.test', address:{city:"Moscow", street:"1 Street", zipcode:"11"}}
+	]
 return (
 	<div>
-	<Card 
-		onClick={(num)=>alert("click!" + num)}
-		width='500px' 
-		height='300px' 
-		variant={CardVariant.primary}
-	>
-		<button>B!</button>
-	</Card>
+		<Card 
+			onClick={(num)=>alert("click!" + num)}
+			width='500px' 
+			height='300px' 
+			variant={CardVariant.primary}
+		>
+			<button>B!</button>
+		</Card>
+		<UserList users={users}/>
 	</div>
 );
 };
---
+++ src/types/types.tsx
@@ -0,0 +1,13 @@
+// Описание глобальных типов
+
+export interface IAddress {
+    street: string;
+    city: string;
+    zipcode: string;
+}
+export interface IUser {
+    id: number;
+    name: string;
+    email: string;
+    address: IAddress;
+}
