434d4e6fa3b4bceed860ba476fe6ed657b4d7cee
Krotesk <igor_98_zukov@mail.ru>
2023-08-15 11:39:32 +0300

    Загрузка данных через axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
 import UserList from './components/UserList';
 import { IUser } from './types/types';
-
+import axios from 'axios'
 const App = () => {
-	const users: IUser[] = [
-		{id:1, name:'Igor', email:'test@mail.test', address:{city:"Moscow", street:"1 Street", zipcode:"11"}},
-		{id:2, name:'Igor', email:'test@mail.test', address:{city:"Moscow", street:"1 Street", zipcode:"11"}}
-	]
+
+	const [users, setUsers] = useState<IUser[]>([]);
+
+	useEffect(()=>{
+		fetchUsers();
+	},[])
+
+	async function fetchUsers(){
+		try {
+			const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+			setUsers(response.data)
+		} catch (error) {
+			alert(error)
+		}
+	}
 return (
 	<div>
 		<Card 
